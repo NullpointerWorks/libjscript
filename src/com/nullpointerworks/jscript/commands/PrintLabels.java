dcd4cf7dd44ce4679ccbe0ef9b5a0b59fcd2b920
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-04-23 10:00:01 +0200

    Update PrintLabels.java

--- src/com/nullpointerworks/jscript/commands/PrintLabels.java
+++ src/com/nullpointerworks/jscript/commands/PrintLabels.java
@@ -8,6 +8,13 @@ package com.nullpointerworks.jscript.commands;
 import com.nullpointerworks.jscript.JScript;
 
 /**
+ * TODO - PrintLabels
+ * 
+ * implement [$DBF] dBase file linking
+ * implement [<VAR>] variables
+ * 
+ */
+/**
  * The A command is used to define the end of the label definition and to set the amount of labels to be printed. (From the manual)<br>
  * 
  * @author Michiel Drost - Nullpointer Works
@@ -17,46 +24,55 @@ public class PrintLabels implements JScript
 {
 	private int amount = 0;
 	private boolean noprint = false;
+	private boolean request = false;
+	private boolean repeat = false;
+	
+	private void reset()
+	{
+		amount = 0;
+		setNoPrint(false);
+		setRequestAmount(false);
+		repeat = false;
+	}
 	
 	public PrintLabels setAmount(int amount)
 	{
-		setNoPrint(false);
-		
+		reset();
 		this.amount=amount;
 		return this;
 	}
 	
 	public PrintLabels setNoPrint(boolean b)
 	{
+		reset();
 		noprint = b;
+		return this;
+	}
+	
+	public PrintLabels setRequestAmount(boolean b)
+	{
+		reset();
+		request = b;
+		return this;
+	}
+	
+	public PrintLabels setRequestAmount(boolean b, boolean r)
+	{
+		reset();
+		request = b;
+		repeat = r;
 		return this;
 	}
 	
 	@Override
 	public String getText()
 	{
-		if (noprint)
-		{
-			return "A [NOPRINT]"; // store in buffer
-		}
+		if (noprint) return "A [NOPRINT]"; // store in buffer
+		if (request && !repeat) return "A [?]"; // request amount on the printer
+		if (request &&  repeat) return "A [?,R]"; // request amount on the printer, and repeat when done
 		
-		
-		
-		
-		
-		
-		
-		
-		if (amount > 0)
-		{
-			return "A "+amount; // print a fixed amount
-		}
-		
-		if (amount < 0)
-		{
-			return "A"; // warning! print infinite
-		}
-		
+		if (amount > 0) return "A "+amount; // print a fixed amount
+		if (amount < 0) return "A"; // warning! print infinite
 		return "A 1"; // default; print 1
 	}
 }

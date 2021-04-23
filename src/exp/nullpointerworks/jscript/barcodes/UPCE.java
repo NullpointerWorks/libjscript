f481b883ea04999e577328aaf48747f1b7de1c9c
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-04-23 11:52:40 +0200

    update

---
+++ src/exp/nullpointerworks/jscript/barcodes/UPCE.java
@@ -0,0 +1,29 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ * (http://unlicense.org/)
+ * Nullpointer Works (2021)
+ */
+package exp.nullpointerworks.jscript.barcodes;
+
+import exp.nullpointerworks.jscript.BarcodeType;
+
+/**
+ * TODO - UPC-E
+ * 
+ * @author Michiel Drost - Nullpointer Works
+ * @see The CAB Programming Manual x4 - page 297
+ */
+public class UPCE implements BarcodeType 
+{
+	
+	
+	
+	
+	
+	@Override
+	public String getText() 
+	{
+		return null;
+	}
+
+}
